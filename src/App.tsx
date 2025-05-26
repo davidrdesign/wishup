beed3ae3c4b7b9901dff06ec13f0c0e2fe29980a
David Robinson <davidrobinson@Davids-Mac-Studio.local>
2025-05-26 17:54:32 -0400

    Update App.tsx to use default exports for MetaSEO, Analytics, and AppRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-// import MetaSEO from './MetaSEO';
-// import Analytics from './Analytics';
+import React from 'react';
 import AppRouter from './AppRouter';
+import MetaSEO from './MetaSEO';
+import Analytics from './Analytics';
 
 export default function App() {
   return (
     <>
-      {/* <MetaSEO /> */}
-      {/* <Analytics /> */}
+      <MetaSEO />
+      <Analytics />
       <AppRouter />
     </>
   );
